bcdeb542e0925c20059dc37416936452341452ab
medericcambier <126482071+medericcambier@users.noreply.github.com>
2024-09-25 13:02:54 +0200

    ennemies

--- Assets/enemis.cs
+++ Assets/enemis.cs
@@ -1,18 +1,23 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemis : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float rapidité;
+    private Transform joueur;
+    public float arreter;
+   
     void Start()
     {
-        
+        joueur = GameObject.FindGameObjectWithTag("Joueur").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
+            transform.position = Vector2.MoveTowards(transform.position, joueur.position, rapidité * Time.deltaTime);  
+     
     }
 }
