d78dc09db36c06a547d9b164c3d67751938e609f
KylianBailly <kylianbailly717@gmail.com>
2024-09-26 23:35:10 +0200

    animation Joueur

--- Assets/Script/Player mvt.cs
+++ Assets/Script/Player mvt.cs
@@ -8,6 +8,7 @@ public class Playermvt : MonoBehaviour
     public Rigidbody2D rb;
     public float Vitesse = 85f;
     public Vector2 mouvement;
+    private Animator Animator; 
 
     // Ajout d'une variable pour la vie du joueur
     public float vie = 100f;
@@ -15,7 +16,7 @@ public class Playermvt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Animator = GetComponent<Animator>();
 
     }
 
@@ -26,6 +27,36 @@ public class Playermvt : MonoBehaviour
         mouvement.y = Input.GetAxisRaw("Vertical");
 
         rb.MovePosition(rb.position + mouvement * Vitesse * Time.deltaTime);
+
+        if (mouvement.y == 1)
+        {
+
+            Animator.SetBool("IsBackward", true);
+
+        }
+        else 
+        {
+            Animator.SetBool("IsBackward", false);
+        }
+
+        if (mouvement.magnitude>0)
+        {
+            Animator.SetBool("IsRunning", true);
+        }
+        else 
+        {
+            Animator.SetBool("IsRunning", false);
+        }
+        if (mouvement.x < 0)
+        {
+            // Inverser l'échelle sur l'axe X pour faire face à gauche
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (mouvement.x > 0)
+        {
+            // Remettre l'échelle normale pour faire face à droite
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
     // M�thode pour infliger des d�g�ts
